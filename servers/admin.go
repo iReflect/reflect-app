4aa19fcfc2277e6b0c7bb3b6410f9f07ed2a7208
anmolghosh <anmol.ghosh@joshtechnologygroup.com>
2017-11-28 17:43:39 +0530

    [#IR-4] Fixed variable name

--- servers/admin.go
+++ servers/admin.go
@@ -8,7 +8,7 @@ import (
 	"github.com/qor/qor"
 
 	userModels "github.com/iReflect/reflect-app/apps/user/models"
-	feeddbackModels "github.com/iReflect/reflect-app/apps/feedback/models"
+	feedbackModels "github.com/iReflect/reflect-app/apps/feedback/models"
 )
 
 type Admin struct {
@@ -29,18 +29,18 @@ func (a *Admin) Router() *http.ServeMux {
 	Admin.AddResource(&userModels.Team{}, &admin.Config{Menu: []string{"User Management"}})
 	Admin.AddResource(&userModels.UserTeamAssociation{}, &admin.Config{Menu: []string{"User Management"}})
 
-	Admin.AddResource(&feeddbackModels.Category{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
-	Admin.AddResource(&feeddbackModels.Skill{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
-	Admin.AddResource(&feeddbackModels.Question{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
-	Admin.AddResource(&feeddbackModels.FeedbackForm{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
-	Admin.AddResource(&feeddbackModels.FeedbackFormContent{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
-	Admin.AddResource(&feeddbackModels.TeamFeedbackForm{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
+	Admin.AddResource(&feedbackModels.Category{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
+	Admin.AddResource(&feedbackModels.Skill{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
+	Admin.AddResource(&feedbackModels.Question{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
+	Admin.AddResource(&feedbackModels.FeedbackForm{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
+	Admin.AddResource(&feedbackModels.FeedbackFormContent{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
+	Admin.AddResource(&feedbackModels.TeamFeedbackForm{}, &admin.Config{Menu: []string{"Feedback Form Management"}})
 
-	Admin.AddResource(&feeddbackModels.Feedback{}, &admin.Config{Menu: []string{"Feedback Management"}})
-	Admin.AddResource(&feeddbackModels.QuestionResponse{}, &admin.Config{Menu: []string{"Feedback Management"}})
-	Admin.AddResource(&feeddbackModels.ResponseComment{}, &admin.Config{Menu: []string{"Feedback Management"}})
+	Admin.AddResource(&feedbackModels.Feedback{}, &admin.Config{Menu: []string{"Feedback Management"}})
+	Admin.AddResource(&feedbackModels.QuestionResponse{}, &admin.Config{Menu: []string{"Feedback Management"}})
+	Admin.AddResource(&feedbackModels.ResponseComment{}, &admin.Config{Menu: []string{"Feedback Management"}})
 	
-	Admin.AddResource(&feeddbackModels.Schedule{}, &admin.Config{Menu: []string{"Schedule Management"}})
+	Admin.AddResource(&feedbackModels.Schedule{}, &admin.Config{Menu: []string{"Schedule Management"}})
 	Admin.MountTo("/admin/", adminRouter)
 
 	return adminRouter
