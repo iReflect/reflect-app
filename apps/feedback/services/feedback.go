287b32c38c45b38af4224555d77f449f5b40f7bc
Anmol Ghosh <anmol.ghosh@joshtechnologygroup.com>
2017-12-14 16:39:26 +0530

    Added FeedbackService and refactored code to use it

---
+++ apps/feedback/services/feedback.go
@@ -0,0 +1,129 @@
+package services
+
+import (
+	"github.com/jinzhu/gorm"
+
+	feedbackModels "github.com/iReflect/reflect-app/apps/feedback/models"
+	feedbackSerializers "github.com/iReflect/reflect-app/apps/feedback/serializers"
+	userModels "github.com/iReflect/reflect-app/apps/user/models"
+)
+
+//FeedbackService ...
+type FeedbackService struct {
+	DB *gorm.DB
+}
+
+// Get feedback by id
+func (service FeedbackService) Get(feedbackID string, userID string) (feedback *feedbackSerializers.FeedbackDetailSerializer,
+	err error) {
+	db := service.DB
+	feedback = new(feedbackSerializers.FeedbackDetailSerializer)
+
+	if err := db.Model(&feedbackModels.Feedback{}).Where("id = ?", feedbackID).
+		Where("by_user_profile_id in (?)",
+		db.Model(&userModels.UserProfile{}).Where("user_id = ?", userID).Select("id").QueryExpr()).
+		Select("id, title, duration_start,duration_end, submitted_at, expire_at, status, feedback_form_id").
+		Scan(&feedback).Error; err != nil {
+		return nil, err
+	}
+
+	feedBackFormContents := []feedbackModels.FeedbackFormContent{}
+
+	if err := db.Model(&feedbackModels.FeedbackFormContent{}).
+		Preload("Skill").
+		Preload("Skill.Questions").
+		Preload("Category").
+		Group("id, category_id").
+		Where("feedback_form_id in (?)", feedback.FeedbackFormID).
+		Find(&feedBackFormContents).Error; err != nil {
+		return nil, err
+	}
+
+	categories := make(map[uint]feedbackSerializers.CategoryDetailSerializer)
+
+	for _, feedBackFormContent := range feedBackFormContents {
+		questionResponses := []feedbackSerializers.QuestionResponseDetailSerializer{}
+		for _, question := range feedBackFormContent.Skill.Questions {
+			questionResponse := feedbackModels.QuestionResponse{}
+			db.Model(questionResponse).
+				Where(feedbackModels.QuestionResponse{
+				FeedbackID:            feedback.ID,
+				QuestionID:            question.ID,
+				FeedbackFormContentID: feedBackFormContent.ID,
+			}).
+				FirstOrCreate(&questionResponse)
+
+			questionResponses = append(questionResponses,
+				feedbackSerializers.QuestionResponseDetailSerializer{
+					ID:         question.ID,
+					Type:       question.Type,
+					Text:       question.Text,
+					Options:    question.Options,
+					Weight:     question.Weight,
+					ResponseID: questionResponse.ID,
+					Response:   questionResponse.Response,
+					Comment:    questionResponse.Comment,
+				})
+		}
+
+		skill := feedbackSerializers.SkillDetailSerializer{
+			ID:           feedBackFormContent.SkillID,
+			Title:        feedBackFormContent.Skill.Title,
+			DisplayTitle: feedBackFormContent.Skill.DisplayTitle,
+			Description:  feedBackFormContent.Skill.Description,
+			Weight:       feedBackFormContent.Skill.Weight,
+			Questions:    questionResponses,
+		}
+
+		categoryID := feedBackFormContent.CategoryID
+		_, exists := categories[categoryID]
+		if exists == false {
+			skills := make(map[uint]feedbackSerializers.SkillDetailSerializer)
+			skills[feedBackFormContent.SkillID] = skill
+
+			categories[categoryID] = feedbackSerializers.CategoryDetailSerializer{
+				ID:          feedBackFormContent.Category.ID,
+				Title:       feedBackFormContent.Category.Title,
+				Description: feedBackFormContent.Category.Description,
+				Skills:      skills,
+			}
+		} else {
+			categories[categoryID].Skills[feedBackFormContent.SkillID] = skill
+		}
+	}
+	feedback.Categories = categories
+	return feedback, nil
+}
+
+// List users Feedback
+func (service FeedbackService) List(userID string, statuses []string) (
+	feedbacks *feedbackSerializers.FeedbackListSerializer,
+	err error) {
+	db := service.DB
+	baseQuery := db.Model(&feedbackModels.Feedback{}).
+		Where("by_user_profile_id in (?)",
+		db.Model(&userModels.UserProfile{}).Where("user_id = ?", userID).Select("id").QueryExpr())
+
+	listQuery := baseQuery
+	if len(statuses) > 0 {
+		listQuery = listQuery.Where("status in (?)", statuses)
+	}
+
+	feedbacks = new(feedbackSerializers.FeedbackListSerializer)
+	if err := listQuery.
+		Preload("Team").
+		Preload("ByUserProfile").
+		Preload("ByUserProfile.User").
+		Preload("ByUserProfile.Role").
+		Preload("ForUserProfile").
+		Preload("ForUserProfile.User").
+		Preload("ForUserProfile.Role").
+		Preload("FeedbackForm").
+		Find(&feedbacks.Feedbacks).Error; err != nil {
+		return nil, err
+	}
+	baseQuery.Where("status = 0").Count(&feedbacks.NewFeedbackCount)
+	baseQuery.Where("status = 1").Count(&feedbacks.DraftFeedbackCount)
+	baseQuery.Where("status = 2").Count(&feedbacks.SubmittedFeedbackCount)
+	return feedbacks, nil
+}
--- controllers/v1/feedback.go
+++ controllers/v1/feedback.go
@@ -4,140 +4,40 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	feedbackModels "github.com/iReflect/reflect-app/apps/feedback/models"
-	feedbackSerializers "github.com/iReflect/reflect-app/apps/feedback/serializers"
-	userModels "github.com/iReflect/reflect-app/apps/user/models"
-	database "github.com/iReflect/reflect-app/db"
+	feedbaclServices "github.com/iReflect/reflect-app/apps/feedback/services"
 )
 
 //FeedbackController ...
 type FeedbackController struct {
+	FeedbackService feedbaclServices.FeedbackService
 }
 
 // Routes for Feedback
 func (ctrl FeedbackController) Routes(r *gin.RouterGroup) {
 	r.GET("/", ctrl.List)
 	r.GET("/:id/", ctrl.Get)
-	//r.PUT("/:id", ctrl.Update)
 }
 
 // Get feedback
 func (ctrl FeedbackController) Get(c *gin.Context) {
 	id := c.Param("id")
-	db, _ := database.GetFromContext(c)
-	feedbackResponse := feedbackSerializers.FeedbackDetailSerializer{}
-	if err := db.Model(&feedbackModels.Feedback{}).Where("id = ?", id).Select("id, title, duration_start, duration_end, submitted_at, expire_at, status, feedback_form_id").
-		Scan(&feedbackResponse).Error; err != nil {
+	feedbackResponse, err := ctrl.FeedbackService.Get(id, "1")
+	if err != nil {
 		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Feedback not found", "error": err})
 		return
 	}
 
-	feedBackFormContents := []feedbackModels.FeedbackFormContent{}
-
-	if err := db.Model(&feedbackModels.FeedbackFormContent{}).
-		Preload("Skill").
-		Preload("Skill.Questions").
-		Preload("Category").
-		Group("id, category_id").
-		Where("feedback_form_id in (?)", feedbackResponse.FeedbackFormID).
-		Find(&feedBackFormContents).Error; err != nil {
-		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Feedback not found", "error": err})
-	}
-
-	var categories = make(map[uint]feedbackSerializers.CategoryDetailSerializer)
-
-	for _, feedBackFormContent := range feedBackFormContents {
-		questionResponses := []feedbackSerializers.QuestionResponseDetailSerializer{}
-		for _, question := range feedBackFormContent.Skill.Questions {
-			questionResponse := feedbackModels.QuestionResponse{}
-			db.Model(questionResponse).
-				Where(feedbackModels.QuestionResponse{
-				FeedbackID:            feedbackResponse.ID,
-				QuestionID:            question.ID,
-				FeedbackFormContentID: feedBackFormContent.ID,
-			}).
-				FirstOrCreate(&questionResponse)
-			questionResponses = append(questionResponses,
-				feedbackSerializers.QuestionResponseDetailSerializer{
-					ID:         question.ID,
-					Type:       question.Type,
-					Text:       question.Text,
-					Options:    question.Options,
-					Weight:     question.Weight,
-					ResponseID: questionResponse.ID,
-					Response:   questionResponse.Response,
-					Comment:    questionResponse.Comment,
-				})
-		}
-
-		skill := feedbackSerializers.SkillDetailSerializer{
-			ID:           feedBackFormContent.SkillID,
-			Title:        feedBackFormContent.Skill.Title,
-			DisplayTitle: feedBackFormContent.Skill.DisplayTitle,
-			Description:  feedBackFormContent.Skill.Description,
-			Weight:       feedBackFormContent.Skill.Weight,
-			Questions:    questionResponses,
-		}
-
-		categoryID := feedBackFormContent.CategoryID
-		_, exists := categories[categoryID]
-		if exists == false {
-			skills := make(map[uint]feedbackSerializers.SkillDetailSerializer)
-			skills[feedBackFormContent.SkillID] = skill
-
-			categories[categoryID] = feedbackSerializers.CategoryDetailSerializer{
-				ID:          feedBackFormContent.Category.ID,
-				Title:       feedBackFormContent.Category.Title,
-				Description: feedBackFormContent.Category.Description,
-				Skills:      skills,
-			}
-		} else {
-			categories[categoryID].Skills[feedBackFormContent.SkillID] = skill
-		}
-	}
-	c.JSON(http.StatusOK, feedbackSerializers.FeedbackDetailSerializer{
-		ID:            feedbackResponse.ID,
-		Title:         feedbackResponse.Title,
-		DurationStart: feedbackResponse.DurationStart,
-		DurationEnd:   feedbackResponse.DurationEnd,
-		SubmittedAt:   feedbackResponse.SubmittedAt,
-		ExpireAt:      feedbackResponse.ExpireAt,
-		Status:        feedbackResponse.Status,
-		Categories:    categories,
-	})
+	c.JSON(http.StatusOK, feedbackResponse)
 }
 
 // List Feedbacks
 func (ctrl FeedbackController) List(c *gin.Context) {
-	db, _ := database.GetFromContext(c)
-	status := c.QueryArray("status")
-	response := feedbackSerializers.FeedbackListSerializer{}
-	baseQuery := db.Model(&feedbackModels.Feedback{}).
-		Where("by_user_profile_id in (?)",
-		db.Model(&userModels.UserProfile{}).Where("user_id = ?", 1).Select("id").QueryExpr())
+	statuses := c.QueryArray("status")
+	response, err := ctrl.FeedbackService.List("1", statuses)
 
-	listQuery := baseQuery
-	if len(status) > 0 {
-		listQuery = listQuery.Where("status in (?)", status)
-
-	}
-
-	if err := listQuery.
-		Preload("Team").
-		Preload("ByUserProfile").
-		Preload("ByUserProfile.User").
-		Preload("ByUserProfile.Role").
-		Preload("ForUserProfile").
-		Preload("ForUserProfile.User").
-		Preload("ForUserProfile.Role").
-		Preload("FeedbackForm").
-		Find(&response.Feedbacks).Error; err != nil {
+	if err != nil {
 		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Feedbacks not found", "error": err})
 		return
 	}
-	baseQuery.Where("status = 0").Count(&response.NewFeedbackCount)
-	baseQuery.Where("status = 1").Count(&response.DraftFeedbackCount)
-	baseQuery.Where("status = 2").Count(&response.SubmittedFeedbackCount)
-	response.Token = state.(string)
 	c.JSON(http.StatusOK, response)
 }
