cfd439ed5ac32cd1f4c8fa5ab8d06a33985664d3
Anmol Ghosh <anmol.ghosh@joshtechnologygroup.com>
2017-12-14 14:35:09 +0530

    [#IR-11] Create Feedback Form GET API (#12)

--- apps/feedback/controllers/feedback.go
+++ apps/feedback/controllers/feedback.go
@@ -15,25 +15,98 @@ import (
 type FeedbackController struct {
 }
 
-//Add Routes
+// Routes for Feedback
 func (ctrl FeedbackController) Routes(r *gin.RouterGroup) {
 	r.GET("/", ctrl.List)
-	//r.GET("/:id", ctrl.Get)
+	r.GET("/:id/", ctrl.Get)
 	//r.PUT("/:id", ctrl.Update)
 }
 
-//Get feedback 
-//func (ctrl FeedbackController) Get(c *gin.Context) {
-//	id := c.Param("id")
-//	db, _ := database.GetFromContext(c)
-//	feedback := feedbackModels.Feedback{}
-//
-//	if err := db.First(&feedback, id).Error; err != nil {
-//		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Feedback not found", "error": err})
-//		return
-//	}
-//	c.JSON(http.StatusOK, feedback)
-//}
+// Get feedback
+func (ctrl FeedbackController) Get(c *gin.Context) {
+	id := c.Param("id")
+	db, _ := database.GetFromContext(c)
+	feedbackResponse := feedbackModels.FeedbackDetailResponse{}
+	if err := db.Model(&feedbackModels.Feedback{}).Where("id = ?", id).Select("id, title, duration_start, duration_end, submitted_at, expire_at, status, feedback_form_id").
+		Scan(&feedbackResponse).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Feedback not found", "error": err})
+		return
+	}
+
+	feedBackFormContents := []feedbackModels.FeedbackFormContent{}
+
+	if err := db.Model(&feedbackModels.FeedbackFormContent{}).
+		Preload("Skill").
+		Preload("Skill.Questions").
+		Preload("Category").
+		Group("id, category_id").
+		Where("feedback_form_id in (?)", feedbackResponse.FeedbackFormID).
+		Find(&feedBackFormContents).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Feedback not found", "error": err})
+	}
+
+	var categories = make(map[uint]feedbackModels.CategorySkillQuestions)
+
+	for _, feedBackFormContent := range feedBackFormContents {
+		questionResponseList := []feedbackModels.QuestionResponseDetail{}
+		for _, question := range feedBackFormContent.Skill.Questions {
+			questionResponse := feedbackModels.QuestionResponse{}
+			db.Model(questionResponse).
+				Where(feedbackModels.QuestionResponse{
+					FeedbackID:            feedbackResponse.ID,
+					QuestionID:            question.ID,
+					FeedbackFormContentID: feedBackFormContent.ID,
+				}).
+				FirstOrCreate(&questionResponse)
+			questionResponseList = append(questionResponseList,
+				feedbackModels.QuestionResponseDetail{
+					ID:         question.ID,
+					Type:       question.Type,
+					Text:       question.Text,
+					Options:    question.Options,
+					Weight:     question.Weight,
+					ResponseID: questionResponse.ID,
+					Response:   questionResponse.Response,
+					Comment:    questionResponse.Comment,
+				})
+		}
+
+		skillQuestionResponse := feedbackModels.SkillQuestionList{
+			ID:           feedBackFormContent.SkillID,
+			Title:        feedBackFormContent.Skill.Title,
+			DisplayTitle: feedBackFormContent.Skill.DisplayTitle,
+			Description:  feedBackFormContent.Skill.Description,
+			Weight:       feedBackFormContent.Skill.Weight,
+			Questions:    questionResponseList,
+		}
+
+		categoryID := feedBackFormContent.CategoryID
+		_, exists := categories[categoryID]
+		if exists == false {
+			skillQuestionMap := make(map[uint]feedbackModels.SkillQuestionList)
+			skillQuestionMap[feedBackFormContent.SkillID] = skillQuestionResponse
+
+			categories[categoryID] = feedbackModels.CategorySkillQuestions{
+				ID:          feedBackFormContent.Category.ID,
+				Title:       feedBackFormContent.Category.Title,
+				Description: feedBackFormContent.Category.Description,
+				Skills:      skillQuestionMap,
+			}
+		} else {
+			categories[categoryID].Skills[feedBackFormContent.SkillID] = skillQuestionResponse
+		}
+	}
+	c.JSON(http.StatusOK, feedbackModels.FeedbackDetailResponse{
+		ID:            feedbackResponse.ID,
+		Title:         feedbackResponse.Title,
+		DurationStart: feedbackResponse.DurationStart,
+		DurationEnd:   feedbackResponse.DurationEnd,
+		SubmittedAt:   feedbackResponse.SubmittedAt,
+		ExpireAt:      feedbackResponse.ExpireAt,
+		Status:        feedbackResponse.Status,
+		Categories:    categories,
+	})
+}
 
 // List Feedbacks
 func (ctrl FeedbackController) List(c *gin.Context) {
@@ -42,7 +115,7 @@ func (ctrl FeedbackController) List(c *gin.Context) {
 	response := feedbackModels.FeedbackListResponse{}
 	baseQuery := db.Model(&feedbackModels.Feedback{}).
 		Where("by_user_profile_id in (?)",
-		db.Model(&userModels.UserProfile{}).Where("user_id = ?", 1).Select("id").QueryExpr())
+			db.Model(&userModels.UserProfile{}).Where("user_id = ?", 1).Select("id").QueryExpr())
 
 	listQuery := baseQuery
 	if len(status) > 0 {
--- apps/feedback/models/feedback.go
+++ apps/feedback/models/feedback.go
@@ -6,31 +6,76 @@ import (
 	"github.com/jinzhu/gorm"
 
 	userModels "github.com/iReflect/reflect-app/apps/user/models"
+	"github.com/iReflect/reflect-app/db/models/fields"
 )
 
 // Feedback represent a submitted/in-progress feedback form by a user
 type Feedback struct {
 	gorm.Model
 	FeedbackForm     FeedbackForm
-	Title            string    `gorm:"type:varchar(255); not null"`
-	FeedbackFormID   uint      `gorm:"not null"`
+	Title            string `gorm:"type:varchar(255); not null"`
+	FeedbackFormID   uint   `gorm:"not null"`
 	ForUserProfile   userModels.UserProfile
 	ForUserProfileID uint
 	ByUserProfile    userModels.UserProfile
-	ByUserProfileID  uint      `gorm:"not null"`
+	ByUserProfileID  uint `gorm:"not null"`
 	Team             userModels.Team
-	TeamID           uint      `gorm:"not null"`
-	Status           int8      `gorm:"default:0; not null"` // TODO Add enum
+	TeamID           uint `gorm:"not null"`
+	Status           int8 `gorm:"default:0; not null"` // TODO Add enum
 	SubmittedAt      *time.Time
 	DurationStart    time.Time `gorm:"not null"`
 	DurationEnd      time.Time `gorm:"not null"`
 	ExpireAt         time.Time `gorm:"not null"`
 }
 
+// FeedbackListResponse lists the feedbacks for a given user
 type FeedbackListResponse struct {
 	NewFeedbackCount       uint
 	DraftFeedbackCount     uint
 	SubmittedFeedbackCount uint
 	Feedbacks              []Feedback
 	Token                  string
+}
+
+// QuestionResponseDetail returns the question response for a particular question
+type QuestionResponseDetail struct {
+	ID         uint
+	Text       string
+	Type       int8
+	Options    fields.JSONB
+	Weight     int
+	ResponseID uint
+	Response   string
+	Comment    string
+}
+
+// SkillQuestionList returns the skill details with a list of questions under that skill
+type SkillQuestionList struct {
+	ID           uint
+	Title        string
+	DisplayTitle string
+	Description  string
+	Weight       int
+	Questions    []QuestionResponseDetail
+}
+
+// CategorySkillQuestions returns the list of questions for a skill of a category
+type CategorySkillQuestions struct {
+	ID          uint
+	Title       string
+	Description string
+	Skills      map[uint]SkillQuestionList
+}
+
+// FeedbackDetailResponse returns the details of a feedback
+type FeedbackDetailResponse struct {
+	ID             uint
+	Title          string
+	DurationStart  time.Time
+	DurationEnd    time.Time
+	SubmittedAt    time.Time
+	ExpireAt       time.Time
+	Status         int8
+	FeedbackFormID uint
+	Categories     map[uint]CategorySkillQuestions
 }
