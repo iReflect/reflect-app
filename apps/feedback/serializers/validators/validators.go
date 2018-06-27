6829d4da4cb34476fe155a75cd9d7ac8f7912863
Himanshu Garg <himanshu.garg@joshtechnologygroup.com>
2018-06-27 16:07:19 +0530

    [#IR-247] Implement methods to fetch sprint, tasks from Pivotal tracker

--- apps/feedback/serializers/validators/validators.go
+++ apps/feedback/serializers/validators/validators.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/jinzhu/gorm"
+	"gopkg.in/go-playground/validator.v8"
 )
 
 //FeedbackValidators is used for registering validators for the feedback app
@@ -12,18 +13,21 @@ type FeedbackValidators struct {
 }
 
 // Register registers all the validators for the feedback serializers
-func (validator FeedbackValidators) Register() {
-	if err := binding.Validator.RegisterValidation("is_valid_submitted_at",
+func (feedbackValidator FeedbackValidators) Register() {
+
+	validatorEngine := binding.Validator.Engine().(*validator.Validate)
+
+	if err := validatorEngine.RegisterValidation("is_valid_submitted_at",
 		IsValidSubmittedAt); err != nil {
 		fmt.Println(err.Error())
 	}
 
-	if err := binding.Validator.RegisterValidation("all_questions_present",
-		IsAllQuestionPresent(validator.DB)); err != nil {
+	if err := validatorEngine.RegisterValidation("all_questions_present",
+		IsAllQuestionPresent(feedbackValidator.DB)); err != nil {
 		fmt.Println(err.Error())
 	}
 
-	if err := binding.Validator.RegisterValidation("is_valid_question_response",
+	if err := validatorEngine.RegisterValidation("is_valid_question_response",
 		IsValidQuestionResponse); err != nil {
 		fmt.Println(err.Error())
 	}
--- apps/tasktracker/providers/pivotal.go
+++ apps/tasktracker/providers/pivotal.go
@@ -3,9 +3,13 @@ package providers
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/iReflect/go-pivotaltracker/v5/pivotal"
 	"github.com/iReflect/reflect-app/apps/tasktracker"
 	"github.com/iReflect/reflect-app/apps/tasktracker/serializers"
-	"gopkg.in/salsita/go-pivotaltracker.v1/v5/pivotal"
+	"github.com/iReflect/reflect-app/libs/utils"
+	"strconv"
+	"strings"
 )
 
 // PivotalTaskProvider ...
@@ -20,8 +24,8 @@ type PivotalConnection struct {
 
 // PivotalConfig ...
 type PivotalConfig struct {
-	Credentials  tasktracker.Credentials `json:"Credentials"`
-	ProjectID    int                     `json:"ProjectID"`
+	Credentials tasktracker.Credentials `json:"Credentials"`
+	ProjectID   string                  `json:"ProjectID"`
 }
 
 // TaskProviderPivotal ...
@@ -100,28 +104,180 @@ func (p *PivotalTaskProvider) ConfigTemplate() (configMap map[string]interface{}
 	return configMap
 }
 
+// getProjectUsers ...
+func (c *PivotalConnection) getProjectUsers() ([]pivotal.Person, error) {
+	projectID, err := strconv.Atoi(c.config.ProjectID)
+	if err != nil {
+		return nil, err
+	}
+
+	projectMemberships, _, err := c.client.Memberships.List(projectID)
+	var projectUsers []pivotal.Person
+	if err != nil {
+		return nil, err
+	}
+
+	for _, projectMembership := range projectMemberships {
+		projectUsers = append(projectUsers, projectMembership.Person)
+	}
+
+	return projectUsers, err
+}
+
+// getUserIDNameMap ...
+func (c *PivotalConnection) getUserIDNameMap() map[int]string {
+	projectUsers, err := c.getProjectUsers()
+
+	if err != nil {
+		return nil
+	}
+
+	userIDNameMap := make(map[int]string)
+	for _, user := range projectUsers {
+		userIDNameMap[user.Id] = user.Name
+	}
+
+	return userIDNameMap
+}
+
+// serializeTickets ...
+func (c *PivotalConnection) serializeTickets(tickets []*pivotal.Story, userIDNameMap map[int]string) (ticketsSerialized []serializers.Task) {
+
+	for _, ticket := range tickets {
+		ticketsSerialized = append(ticketsSerialized, *c.serializeTicket(ticket, userIDNameMap))
+	}
+
+	return ticketsSerialized
+}
+
+// serializeTicket ...
+func (c *PivotalConnection) serializeTicket(ticket *pivotal.Story, userIDNameMap map[int]string) *serializers.Task {
+	ticketID := strconv.Itoa(ticket.Id)
+	task := &serializers.Task{
+		Key:             ticketID,
+		TrackerUniqueID: ticketID,
+		Summary:         ticket.Name,
+		Description:     ticket.Description,
+		Type:            ticket.Type,
+		Estimate:        ticket.Estimate,
+		Status:          ticket.State,
+		ProjectID:       c.config.ProjectID,
+		Priority:        "",
+	}
+
+	// Set Assignee of a task, since PT has owners (multiple) so we can set it to a comma separated list of Owner names
+	if userIDNameMap != nil {
+		var owners []string
+		for _, ownerId := range ticket.OwnerIds {
+			if ownerName, isPresent := userIDNameMap[ownerId]; isPresent {
+				owners = append(owners, ownerName)
+			}
+		}
+		task.Assignee = strings.Join(owners, ", ")
+	}
+	return task
+}
+
 // GetTaskList ...
 func (c *PivotalConnection) GetTaskList(ticketKeys []string) []serializers.Task {
-	return nil
+	if len(ticketKeys) == 0 {
+		return nil
+	}
+
+	filterQuery := fmt.Sprintf("id:%s", strings.Join(ticketKeys, ","))
+
+	projectID, err := strconv.Atoi(c.config.ProjectID)
+	if err != nil {
+		return nil
+	}
+
+	stories, err := c.client.Stories.List(projectID, filterQuery)
+	if err != nil {
+		utils.LogToSentry(err)
+		return nil
+	}
+
+	return c.serializeTickets(stories, c.getUserIDNameMap())
 }
 
 // GetTask ...
 func (c *PivotalConnection) GetTask(ticketKey string) (*serializers.Task, error) {
-	return nil, nil
+	ticketID, err := strconv.Atoi(ticketKey)
+	// Since the ticket ids are always number in Pivotal, if a value is not
+	if err != nil {
+		utils.LogToSentry(err)
+		return nil, nil
+	}
+	projectID, err := strconv.Atoi(c.config.ProjectID)
+	if err != nil {
+		return nil, err
+	}
+
+	story, _, err := c.client.Stories.Get(projectID, ticketID)
+	if err != nil {
+		utils.LogToSentry(err)
+		return nil, err
+	}
+
+	return c.serializeTicket(story, c.getUserIDNameMap()), nil
 }
 
 // GetSprint ...
 func (c *PivotalConnection) GetSprint(sprintID string) *serializers.Sprint {
-	return nil
+	iterationNumber, err := strconv.Atoi(sprintID)
+	if err != nil {
+		utils.LogToSentry(err)
+		return nil
+	}
+	projectID, err := strconv.Atoi(c.config.ProjectID)
+	if err != nil {
+		return nil
+	}
+	iteration, _, err := c.client.Iterations.Get(projectID, iterationNumber)
+	if err != nil {
+		utils.LogToSentry(err)
+		return nil
+	}
+	return &serializers.Sprint{
+		ID:       sprintID,
+		BoardID:  "",
+		Name:     fmt.Sprintf("Iteration-%v", strconv.Itoa(iteration.Number)),
+		FromDate: iteration.Start,
+		ToDate:   iteration.Finish,
+	}
 }
 
 // GetSprintTaskList ...
 func (c *PivotalConnection) GetSprintTaskList(sprint serializers.Sprint) []serializers.Task {
-	return nil
+	iterationNumber, err := strconv.Atoi(sprint.ID)
+	if err != nil {
+		utils.LogToSentry(err)
+		return nil
+	}
+	projectID, err := strconv.Atoi(c.config.ProjectID)
+	if err != nil {
+		return nil
+	}
+	iteration, _, err := c.client.Iterations.Get(projectID, iterationNumber)
+	if err != nil {
+		utils.LogToSentry(err)
+		return nil
+	}
+
+	var storyIDs []string
+	for _, story := range iteration.Stories {
+		storyIDs = append(storyIDs, strconv.Itoa(story.Id))
+	}
+
+	return c.GetTaskList(storyIDs)
 }
 
 // ValidateConfig validates if the provided API Token and ProjectID are correct
 func (c *PivotalConnection) ValidateConfig() error {
-	_, _, err := c.client.Projects.Get(c.config.ProjectID)
+	projectID, err := strconv.Atoi(c.config.ProjectID)
+	if err != nil {
+		return nil
+	}
+	_, _, err = c.client.Projects.Get(projectID)
 	return err
 }
