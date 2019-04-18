d894f6dfe312d2be5a033f190b08a7be8d802c56
chahat-arora <chahat.arora@joshtechnologygroup.com>
2019-04-18 17:31:37 +0530

    [#IR-331] review fixes

--- apps/retrospective/services/sprint_task.go
+++ apps/retrospective/services/sprint_task.go
@@ -134,42 +134,58 @@ func (service SprintTaskService) Update(sprintTaskID string, retroID string, spr
 
 // Delete ...
 func (service SprintTaskService) Delete(sprintTaskID string, retroID string, sprintID string) (int, error) {
-	gormDB := service.DB
+	tx := service.DB.Begin()
 
 	var sprintTask retroModels.SprintTask
-	err := gormDB.Model(retroModels.SprintTask{}).Where("id = ?", sprintTaskID).Scan(&sprintTask).Error
+	err := tx.Model(retroModels.SprintTask{}).Where("id = ?", sprintTaskID).Scan(&sprintTask).Error
 	if err != nil {
+		tx.Rollback()
 		return http.StatusInternalServerError, err
 	}
 
+	// This will give count of total sprint-task for this specific task.
 	var sprintTasksCount int
-	err = gormDB.Model(&retroModels.SprintTask{}).Where("task_id = ?", sprintTask.TaskID).Count(&sprintTasksCount).Error
+	err = tx.Model(&retroModels.SprintTask{}).Where("task_id = ?", sprintTask.TaskID).Count(&sprintTasksCount).Error
 	if err != nil {
+		tx.Rollback()
+		return http.StatusInternalServerError, err
+	}
+
+	// delete sprintMemberTask
+	err = tx.Where("sprint_task_id = ?", sprintTaskID).Delete(retroModels.SprintMemberTask{}).Error
+	if err != nil {
+		tx.Rollback()
 		return http.StatusInternalServerError, err
 	}
 
 	// delete sprintTask.
-	err = gormDB.Where("id = ?", sprintTaskID).Delete(retroModels.SprintTask{}).Error
+	err = tx.Where("id = ?", sprintTaskID).Delete(retroModels.SprintTask{}).Error
 	if err != nil {
+		tx.Rollback()
 		return http.StatusInternalServerError, err
 	}
-	// delete sprintMemberTask
-	err = gormDB.Where("sprint_task_id = ?", sprintTaskID).Delete(retroModels.SprintMemberTask{}).Error
-	if err != nil {
-		return http.StatusInternalServerError, err
-	}
+
 	// if no other sprint contains this task then delete task and taskKeyMap.
 	if sprintTasksCount == 1 {
+
 		// Delete task.
-		err = gormDB.Where("id = ?", sprintTask.TaskID).Delete(retroModels.Task{}).Error
+		err = tx.Where("id = ?", sprintTask.TaskID).Delete(retroModels.Task{}).Error
 		if err != nil {
+			tx.Rollback()
 			return http.StatusInternalServerError, err
 		}
+
 		// Delete task key map.
-		err = gormDB.Where("task_id = ?", sprintTask.TaskID).Delete(retroModels.TaskKeyMap{}).Error
+		err = tx.Where("task_id = ?", sprintTask.TaskID).Delete(retroModels.TaskKeyMap{}).Error
 		if err != nil {
+			tx.Rollback()
 			return http.StatusInternalServerError, err
 		}
+	}
+	err = tx.Commit().Error
+	if err != nil {
+		tx.Rollback()
+		return http.StatusInternalServerError, err
 	}
 	return http.StatusOK, nil
 }
